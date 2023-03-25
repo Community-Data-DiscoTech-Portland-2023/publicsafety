fd43cdf6d438e378ce90a43351d0dff2f9d1be66
colathro <colton@spacedoglabs.com>
2023-03-25 11:41:37 -0700

    google maps website

---
+++ visualizations/where-to-park/index.js
@@ -0,0 +1,34 @@
+let map;
+
+function initMap() {
+  var heatmapData = [
+    new google.maps.LatLng(37.782, -122.447),
+    new google.maps.LatLng(37.782, -122.445),
+    new google.maps.LatLng(37.782, -122.443),
+    new google.maps.LatLng(37.782, -122.441),
+    new google.maps.LatLng(37.782, -122.439),
+    new google.maps.LatLng(37.782, -122.437),
+    new google.maps.LatLng(37.782, -122.435),
+    new google.maps.LatLng(37.785, -122.447),
+    new google.maps.LatLng(37.785, -122.445),
+    new google.maps.LatLng(37.785, -122.443),
+    new google.maps.LatLng(37.785, -122.441),
+    new google.maps.LatLng(37.785, -122.439),
+    new google.maps.LatLng(37.785, -122.437),
+    new google.maps.LatLng(37.785, -122.435),
+  ];
+  var sanFrancisco = new google.maps.LatLng(37.774546, -122.433523);
+
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: sanFrancisco,
+    zoom: 8,
+  });
+
+  var heatmap = new google.maps.visualization.HeatmapLayer({
+    data: heatmapData,
+  });
+
+  heatmap.setMap(map);
+}
+
+window.initMap = initMap;
